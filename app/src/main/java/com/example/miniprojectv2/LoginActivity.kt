8d6165c6ddd65232328af8080e67be02da56de13
Ryan <ryanradithya2005@gmail.com>
2025-10-15 14:17:19 +0700

    Checkpoint 7: Add hardcoded login activity (user)

---
+++ app/src/main/java/com/example/miniprojectv2/LoginActivity.kt
@@ -0,0 +1,45 @@
+package com.example.miniprojectv2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameInput = findViewById<EditText>(R.id.username_input)
+        val passwordInput = findViewById<EditText>(R.id.password_input)
+        val loginButton = findViewById<Button>(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString().trim()
+            val password = passwordInput.text.toString().trim()
+
+            // hardcoded auth
+            when {
+                username == "Ryan" && password == "Ryan123" -> {
+                    Toast.makeText(this, "Login sukses! Selamat datang, Ryan", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+//                username == "Penjual" && password == "Sell123" -> {
+//                    Toast.makeText(this, "Login Penjual berhasil!", Toast.LENGTH_SHORT).show()
+//                    val intent = Intent(this, SellerActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+//                }
+
+                else -> {
+                    Toast.makeText(this, "Username atau password salah!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
