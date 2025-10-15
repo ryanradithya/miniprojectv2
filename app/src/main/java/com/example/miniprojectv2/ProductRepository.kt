c4f9122d8971605de7c48217effd556ff3a18db3
TipenHans <steven190105@gmail.com>
2025-10-15 21:27:31 +0700

    tipen - produk - ganti foto + nambahin category

--- app/src/main/java/com/example/miniprojectv2/ProductRepository.kt
+++ app/src/main/java/com/example/miniprojectv2/ProductRepository.kt
@@ -8,43 +8,66 @@ object ProductRepository {
             1200000,
             "android.resource://com.example.miniprojectv2/drawable/kamera_a",
             10,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend."),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Kamera Analog"),
         Product(
             "Kamera Analog B",
             900000,
             "android.resource://com.example.miniprojectv2/drawable/kamera_a",
             10,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend."),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Kamera Analog"),
         Product(
             "Kamera Analog C",
             750000,
             "android.resource://com.example.miniprojectv2/drawable/kamera_a",
             10,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend."),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Kamera Analog"),
         Product("Film Kodak Gold",
             120000,
             "android.resource://com.example.miniprojectv2/drawable/roll_a",
             10,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend."),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Roll Film"),
         Product(
             "Lensa 50mm f/1.8",
             850000,
-            "android.resource://com.example.miniprojectv2/drawable/roll_a",
+            "android.resource://com.example.miniprojectv2/drawable/lensa_1",
             10,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend."),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Lensa Analog"),
         Product(
             "Tas Kamera Vintage",
-            250000,
-            "android.resource://com.example.miniprojectv2/drawable/roll_a",
+            90000,
+            "android.resource://com.example.miniprojectv2/drawable/tas_1",
             10,
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.")
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Tas Kamera")
     )
 
     // Produk rekomendasi (fix, tidak bisa diubah runtime, jadi pakai list biasa)
     val rekomendasiProduk = listOf(
-        Product("Film Kodak Gold", 120000, "android.resource://com.example.miniprojectv2/drawable/roll_a", 10, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend."),
-        Product("Lensa 50mm f/1.8", 850000, "android.resource://com.example.miniprojectv2/drawable/roll_a", 10, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend."),
-        Product("Tas Kamera Vintage", 250000, "android.resource://com.example.miniprojectv2/drawable/roll_a", 10, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.")
+        Product("Film Kodak Gold",
+            120000,
+            "android.resource://com.example.miniprojectv2/drawable/roll_a",
+            10,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Roll Film"),
+        Product(
+            "Lensa 50mm f/1.8",
+            850000,
+            "android.resource://com.example.miniprojectv2/drawable/lensa_1",
+            10,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Lensa Analog"),
+        Product(
+            "Tas Kamera Vintage",
+            90000,
+            "android.resource://com.example.miniprojectv2/drawable/tas_1",
+            10,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus lorem tortor, sit amet sodales nisl sagittis auctor. Suspendisse pretium facilisis tincidunt. Phasellus non pretium nisi. Nunc lacus nisi, eleifend.",
+            "Tas Kamera")
     )
 
     // ✅ Mendapatkan semua produk utama
