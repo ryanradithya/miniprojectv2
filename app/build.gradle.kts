dfca58708b71638b519c5aedca72957f3bbc149b
Ryan <ryanradithya2005@gmail.com>
2025-10-16 13:34:57 +0700

    Login Page UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
 //    implementation("com.google.android.material:material:1.9.0")
 //    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    implementation("com.google.android.material:material:1.12.0")
+
     // Jetpack Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
